5a5110b6af914329160c7ff288c9eb9a59d3b54a
nicholasraynes <nicholasraynes@yahoo.com>
2026-04-28 12:31:11 -0400

    feat: added full command and args to debug log for command runner

--- src/utils/run_command.rs
+++ src/utils/run_command.rs
@@ -1,25 +1,31 @@
 use r_log::Logger;
-use std::{ffi::OsStr, process::Command};
+use std::{ffi::OsStr, fmt::Display, process::Command};
 
 use crate::Alert;
 
 pub fn run_command<I, S>(command: &str, args: I, logger: Option<&Logger>) -> Result<String, Alert>
 where
     I: IntoIterator<Item = S>,
-    S: AsRef<OsStr>,
+    S: AsRef<OsStr> + Display,
 {
-    let output = Command::new(command).args(args).output()?;
+    let mut log_message = command.to_string();
+    let cpargs: Vec<S> = args.into_iter().collect();
+    for arg in &cpargs {
+        log_message = format!("{} {}", log_message, arg);
+    }
+    let output = Command::new(command).args(cpargs).output()?;
     let stdout = String::from_utf8(output.stdout)?;
     let stderr = String::from_utf8(output.stderr)?;
     let status = output.status.code().ok_or(&stderr)?;
+
     if status == 0 {
         if let Some(l) = logger {
-            l.debug(&stdout);
+            l.debug(&format!("{} {}\n", log_message, stdout));
         }
         return Ok(stdout);
     }
     if let Some(l) = logger {
-        l.debug(&stderr);
+        l.debug(&format!("{} {}\n", log_message, stderr));
     }
     Err(Alert::from(stderr))
 }
