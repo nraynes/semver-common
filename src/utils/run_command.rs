5bee936737e7e179ceba6370822fd625c91d1588
nicholasraynes <nicholasraynes@yahoo.com>
2026-04-28 12:11:25 -0400

    feat: added logger to run command

--- src/utils/run_command.rs
+++ src/utils/run_command.rs
@@ -1,8 +1,9 @@
+use r_log::Logger;
 use std::{ffi::OsStr, process::Command};
 
 use crate::Alert;
 
-pub fn run_command<I, S>(command: &str, args: I) -> Result<String, Alert>
+pub fn run_command<I, S>(command: &str, args: I, logger: Option<&Logger>) -> Result<String, Alert>
 where
     I: IntoIterator<Item = S>,
     S: AsRef<OsStr>,
@@ -12,7 +13,13 @@ where
     let stderr = String::from_utf8(output.stderr)?;
     let status = output.status.code().ok_or(&stderr)?;
     if status == 0 {
+        if let Some(l) = logger {
+            l.debug(&stdout);
+        }
         return Ok(stdout);
+    }
+    if let Some(l) = logger {
+        l.debug(&stderr);
     }
     Err(Alert::from(stderr))
 }
